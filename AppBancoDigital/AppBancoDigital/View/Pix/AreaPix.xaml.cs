db5ca7581a1f97aab3251b46bfe1e2786e3af6aa
Usuario <Usuario@CPD02-M13>
2023-10-10 15:46:44 -0300

    area pix ok

--- AppBancoDigital/AppBancoDigital/View/Pix/AreaPix.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/Pix/AreaPix.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AppBancoDigital.Model;
+using AppBancoDigital.View.PopUp;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -19,16 +21,33 @@ namespace AppBancoDigital.View.Pix
             if (App.DadosChavePix.Chave == null)
             {
                 txt_chave.Text = "Você ainda não possui chave pix";
+                btn_criar.Text = "Criar Chave Pix";
+                txt_tipo.IsVisible = false;
+                btn_copiar.IsVisible = false;
             }
             else
             {
                 txt_chave.Text = App.DadosChavePix.Chave.ToString();
+                btn_criar.Text = "Alterar Chave Pix";
             }
         }
 
-        private async void btn_criar_Clicked(object sender, EventArgs e)
+        private void btn_criar_Clicked(object sender, EventArgs e)
         {
-           
+            if (App.DadosChavePix.Chave == null)
+            {
+                App.Current.MainPage = new PixAdd();
+            }
+            else
+            {
+                App.Current.MainPage = new PixAdd();
+                //ainda não tem
+            }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Clipboard.SetTextAsync(App.DadosChavePix.Chave);
         }
     }
 }
--- AppBancoDigital/AppBancoDigital/View/PopUp/PixAdd.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/PopUp/PixAdd.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using AppBancoDigital.Model;
+using AppBancoDigital.View.Pix;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,95 @@ namespace AppBancoDigital.View.PopUp
         public PixAdd()
         {
             InitializeComponent();
+
+            
+        }
+
+        private void chk_cpf_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (chk_cpf.IsChecked == true)
+            {
+                txt_chave.Placeholder = "000.000.000-00";
+                chk_email.IsChecked = false;
+                chk_telefone.IsChecked = false;
+
+                txt_chave.Text = App.DadosCorrentista.Cpf;
+                txt_chave.IsEnabled = false;
+
+                chk_telefone.IsEnabled = true;
+                chk_email.IsEnabled = true;
+                chk_cpf.IsEnabled = false;
+            }
+        }
+
+        private async void chk_email_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+
+            if (chk_email.IsChecked == true)
+            {
+                txt_chave.Placeholder = "email@email.com";
+                chk_cpf.IsChecked = false;
+                chk_telefone.IsChecked = false;
+                txt_chave.Text = null; 
+                txt_chave.IsEnabled = true;
+
+                txt_chave.Text = App.DadosCorrentista.Email;
+                await DisplayAlert("!", "Por favor confirme seu email", "OK");
+
+                chk_telefone.IsEnabled = true;
+                chk_email.IsEnabled = false;
+                chk_cpf.IsEnabled = true;
+            }
+        }
+
+        private void chk_telefone_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (chk_telefone.IsChecked == true)
+            {
+                txt_chave.Placeholder = "00000-0000";
+                chk_email.IsChecked = false;
+                chk_cpf.IsChecked = false;
+                txt_chave.Text = null;
+                txt_chave.IsEnabled = true;
+
+                chk_telefone.IsEnabled = false;
+                chk_email.IsEnabled = true;
+                chk_cpf.IsEnabled = true;
+
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+             App.Current.MainPage = new AreaPix();
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            if(txt_chave.Text != null)
+            {
+                App.DadosChavePix.Chave = txt_chave.Text;
+
+
+                if(chk_cpf.IsChecked == true)
+                {
+                    App.DadosChavePix.Tipo = "cpf";
+                }
+                else if (chk_telefone.IsChecked == true) 
+                {
+                    App.DadosChavePix.Tipo = "telefone";
+                }
+                else if (chk_email.IsChecked == true) 
+                { 
+                    App.DadosChavePix.Tipo = "email";
+                }
+
+                App.Current.MainPage = new AreaPix();
+            }
+            else
+            {
+                await DisplayAlert("Campo vazio", "Por Favor informe a chave", "ok");
+            }
         }
     }
 }
